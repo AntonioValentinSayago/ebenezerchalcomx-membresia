1538f33235425d1f4ee1ff1373cb9b8b0f58116f
AntonioValentinSayago <cesar_anto30@hotmail.com>
2025-08-25 15:50:11 -0600

    Configuracion de la Base de datos para Hostinger

--- app/config/config.php
+++ app/config/config.php
@@ -1,14 +1,21 @@
 <?php
 // app/config/config.php
 // Ajusta estas constantes a tu entorno
+/**
+ * define('DB_HOST', 'localhost');
+ * define('DB_NAME', 'church_db');
+ * define('DB_USER', 'root');
+  * define('DB_PASS', '123456789');
+ */
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'church_db');
-define('DB_USER', 'root');
-define('DB_PASS', '123456789');
+define('DB_NAME', 'u735483186_ebenezerchalco');
+define('DB_USER', 'u735483186_ebenezerchalco');
+define('DB_PASS', '=SymfKiL#GI5');
 
 // URL base (sin slash final). Ej: http://localhost/church-mvc-php/public
 // Si usas XAMPP/MAMP, pon la URL correspondiente
-define('BASE_URL', getenv('BASE_URL') ?: 'http://localhost:3000/public');
+// define('BASE_URL', getenv('BASE_URL') ?: 'http://localhost:3000/public');
+define('BASE_URL', 'https://' . $_SERVER['HTTP_HOST']);
 
 // Opciones generales
 date_default_timezone_set('America/Mexico_City');
