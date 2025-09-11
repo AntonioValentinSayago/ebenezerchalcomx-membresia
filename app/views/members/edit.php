dec795b0b33a66fee4e168885a34e07585ba8ad0
AntonioValentinSayago <cesar_anto30@hotmail.com>
2025-09-11 14:12:23 -0600

    Formulario y tablas autilizados

--- app/views/members/edit.php
+++ app/views/members/edit.php
@@ -1,4 +1,4 @@
 <?php
 $base = rtrim(BASE_URL, '/');
-$action = "$base/index.php?controller=members&action=update&id=" . urlencode(isset($member->id) ? $member->id : '');
+$action = "$base/index.php?controller=members&action=update&id=" . urlencode($member->id);
 include __DIR__ . '/form.php';
